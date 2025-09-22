84dacae9c7959d00d7f3be6a40714b8fdaed1751
BOWDHINI <bowdhinipyata217@gmail.com>
2025-09-22 15:18:11 +0530

    inital

---
+++ functions.js
@@ -0,0 +1,11 @@
+//to perform the task 
+function add() { 
+var a=67;
+var t=78;
+document 
+console.log(a+b);
+
+}
+add();
+const add=(a,b)=>console.log(a+b);add(a+b)
+add(78,90);4                                     
